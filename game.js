0db42297563e69a0fad1cb52c77ea1f8c2214a94
Nikhila Lanke <115094180+nikhila-lanke@users.noreply.github.com>
2025-10-04 12:37:33 +0530

    Update game.js

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
             question : loadedQuestion.question
         };
         const answerChoices = [...loadedQuestion.incorrect_answers];
-        formattedQuestion.answer=Math.floor(Math.random()*3)+1;
+        formattedQuestion.answer=Math.floor(Math.random()*4)+1;
         answerChoices.splice(
             formattedQuestion.answer-1, 0, 
             loadedQuestion.correct_answer
@@ -112,6 +112,7 @@ incrementScore = num =>{
     score+=num;
     scoreText.innerText=score;
 }
+
 
 
 
